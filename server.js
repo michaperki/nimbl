183cccf24bda212c1ba0df396137c359cff6368c
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2024-02-16 19:20:29 -0600

    update server port to reference .env

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const { createServer } = require('node:http');
 
 const app = express();
-const server = createServer(app);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
